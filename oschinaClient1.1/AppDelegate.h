184a54b1f16621fd44137d25323d4de277e8cc19
boai <boai@boaimatoiMac.local>
2013-08-19 16:07:39 +0800

    Initial Commit

---
+++ oschinaClient1.1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  oschinaClient1.1
+//
+//  Created by boai on 13-8-19.
+//  Copyright (c) 2013年 bravetorun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ oschinaClient1.1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  oschinaClient1.1
+//
+//  Created by boai on 13-8-19.
+//  Copyright (c) 2013年 bravetorun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
